a10beef7e7fffce2a0d54bc2fae7f21ede64bab0
Christopher M <christopher_mao@branson.org>
2025-10-28 22:16:33 -0700

    update agent ABCs

--- src/agents/on_policy_agents/policy_agent.py
+++ src/agents/on_policy_agents/policy_agent.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from typing import Dict, Tuple
 import torch
 
 from ..base_agent import BaseAgent
@@ -8,5 +9,16 @@ from ..base_agent import BaseAgent
 class PolicyAgent(BaseAgent, ABC):
     entropy_coef: float
 
-    
-    
+    @abstractmethod
+    def find_value(self, state: torch.Tensor) -> torch.Tensor:
+        pass
+
+    # Override parent select_action method to change return type
+    @abstractmethod
+    def select_action(self, state: torch.Tensor, is_training: bool = True) -> Tuple[int, torch.Tensor]:
+        pass
+
+    # Override parent update method to change parameters 
+    @abstractmethod
+    def update(self, states, returns, advantages, log_prob=None, identifier=None) -> Dict[str, float]:
+        pass
